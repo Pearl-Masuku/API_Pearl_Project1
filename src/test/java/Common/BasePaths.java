904bb2bc99ab5b992c43051afa99abd90c44b86a
Pearl-Masuku <pthuso@yahoo.com>
2025-10-28 19:57:36 +0200

    created common package

---
+++ src/test/java/Common/BasePaths.java
@@ -0,0 +1,10 @@
+package Common;
+
+public class BasePaths {
+
+    public static String NdosiBaseUrl = "https://www.ndosiautomation.co.za/API";
+    public static String WeatherBaseURl = "https://weather";
+    //public static String WeatherBaseURl = "https://weather";
+    //public static String WeatherBaseURl = "https://weather";
+
+}
